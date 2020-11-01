7f5f936072c168c16fe147ed271958266a095a8b
Ritwik <ritwikghosh1980@gmail.com>
2020-11-02 05:16:08 +0530

    Prepare to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 connectDB();
 
 //Init Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -15,11 +16,21 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === production) {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (err) => {
-    if (err) return console.log('ERROR');
-    else {
-        return console.log(`Server running on port ${PORT}...`)
-    }
-})
+	if (err) return console.log('ERROR');
+	else {
+		return console.log(`Server running on port ${PORT}...`);
+	}
+});
